87d67bcc68f9f0bb96e2b8ec1f099428bb86e503
Jan Bruedigam <jan.bruedigam@t-online.de>
2020-08-18 14:13:26 +0200

    Implement e2

--- src/ACRLab.jl
+++ src/ACRLab.jl
@@ -7,8 +7,17 @@ using ConstrainedDynamicsVis
 using Interact
 using Blink
 
-export main
+export main,
+    Controller1,
+    Controller2,
+    Controller3,
+    Controller4
 
+
+include("e1.jl")
+include("e2.jl")
+include("e3.jl")
+include("e4.jl")
 include("functions.jl")
 include("gui.jl")
 
--- src/e2.jl
+++ src/e2.jl
@@ -1,6 +1,44 @@
+mutable struct Controller2 <: Controller
+    Δt::Real
+    θprev::Real
+    integrator::Real
+    F::Function
+    control!::Function
+
+    function Controller2(F,Δt)
+
+        new(Δt,0.0,0.0,F,control!)
+    end
+end
+
+function control!(mechanism,controller::Controller2,k)
+    Δt = controller.Δt
+    F = controller.F
+    pend = mechanism.bodies[2]
+
+    q = pend.state.qc
+    θ = (rotation_angle(q) * rotation_axis(q))[1]
+    if k == 1
+        θprev = θ
+    else
+        θprev = controller.θprev
+    end
+
+    e = θ
+    de = (θ-θprev)/Δt
+    controller.integrator += e*Δt
+    E = controller.integrator
+  
+    u = [F(e,E,de)]
+    setForce!(mechanism, geteqconstraint(mechanism, 3), u)
+
+    return
+end
+
 function run2!(str)
     open("Files/E2.jl",create=true,write=true) do file
-        write(file,"# Parameters
+        write(file,"
+        # Parameters
         ex = [1.0;0.0;0.0]
         ey = [0.0;1.0;0.0]
         m1 = 0.135
@@ -10,40 +48,39 @@ function run2!(str)
         L1 = 0.280
         L2 = 0.270
         M = 4.5
-        
+
         cartshape = Box(0.1, 0.5, 0.1, M)
         pend1shape = Box(0.1, 0.1, L1, L1)
         pend2shape = Box(0.1, 0.1, L2, L2)
-        
+
         p1 = [0.0;0.0;l1] # joint connection point
         p2 = [0.0;0.0;l2] # joint connection point
-        
+
         # Desired orientation
         θ1 = 0
         θ2 = 0
-        
+
         # Links
         origin = Origin{Float64}()
         cart = Body(cartshape)
         pend1 = Body(pend1shape)
         pend2 = Body(pend2shape)
-        
+
         # Constraints
         joint1 = EqualityConstraint(Prismatic(origin, cart, ey))
         joint2 = EqualityConstraint(Revolute(cart, pend1, ex; p2 = -p2))
-        
+
         links = [cart;pend1]
         constraints = [joint1;joint2]
         shapes = [cartshape;pend1shape]
-        
-        
+
+
         mech = Mechanism(origin, links, constraints, shapes = shapes, Δt = 0.01)
         setPosition!(origin,cart,Δx = [0;0.0;0])
-        setPosition!(cart,pend1,p2 = -p2, Δq = UnitQuaternion(RotX(pi)))
-        
-        F(theta,dtheta,Vis) = 0
-        sgn(x) = x==0 ? 1 : sign(x)
-        
+        setPosition!(cart,pend1,p2 = -p2, Δq = UnitQuaternion(RotX(0.01)))
+
+        F(e,E,de) = 0
+
         ### Begin Student Input
 
         ")
@@ -52,21 +89,17 @@ function run2!(str)
         
         ### End Student Input
 
-        jointid = joint1.id
-        function controller!(mechanism,k)
-            q = pend1.state.qc
-            θ = (rotation_angle(q) * rotation_axis(q))[1]
-            ω = pend1.state.ωc[1]
-            V = 1/3*m1*L1^2*ω^2 + m1*9.81*l1*(1+cos(θ))
-          
-            u = [F(θ,ω,V)]
-            setForce!(mechanism, geteqconstraint(mechanism, jointid), u)
-        end
-        
+        controller = Controller2(F,0.01)
+
         steps = Base.OneTo(1000)
         storage = Storage{Float64}(steps,2)
+
+        try
+            simulate!(mech,storage,controller,record = true)
+        catch
+            @info \"Unstable behavior\"
+        end
         
-        storage = simulate!(mech,storage,controller!,record = true)
         visualize(mech,storage,shapes)")
     end
 
