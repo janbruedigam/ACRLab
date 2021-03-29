d6cd13796431b70812211503d3b6590da43cf19b
janbruedigam <jan.bruedigam@t-online.de>
2021-03-29 20:15:45 +0200

    Seperate tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,19 @@ using SafeTestsets
 
 mkpath("Files")
 
-@safetestset "Simulation Tests" begin
+@safetestset "E1 Tests" begin
     include("e1_test.jl")
+end
+
+@safetestset "E2 Tests" begin
     include("e2_test.jl")
+end
+
+@safetestset "E3 Tests" begin
     include("e3_test.jl")
+end
+
+@safetestset "E4 Tests" begin
     include("e4_test.jl")
 end
 
